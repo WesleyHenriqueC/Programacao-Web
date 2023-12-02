23ec1dc67bf51b4543feb5258409d3bc30b505e1
WesleyHenriqueC <95238638+WesleyHenriqueC@users.noreply.github.com>
2023-12-02 17:49:57 -0300

    Add files via upload

---
+++ JogoDeBaralho/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Titulo from './components/Titulo/Titulo';
+import Jogo from './components/Jogo/Jogo';
+
+function App() {
+  return (
+    <div>
+      <Titulo />
+      <Jogo />
+    </div>
+  )
+}
+
+export default App
---
+++ JogoDeBaralho/src/components/botaoRegras/botaoRegras.jsx
@@ -0,0 +1,7 @@
+import "./botaoRegras.css";
+
+export default function BotaoRegras(){
+    return(
+        <button className="ButaoRegras">Regras</button>
+    )
+}
