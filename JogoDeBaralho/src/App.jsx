a74fcf5f6a738e803cb5d2e96275783303c2b392
WesleyHenriqueC <95238638+WesleyHenriqueC@users.noreply.github.com>
2023-12-05 21:32:32 -0300

    Add files via upload

---
+++ JogoDeBaralho/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import TelaInicial from './components/TelaInicial/TelaInicial';
+import ListaDeCartas from './components/Regras/ListaDeCartas.jsx'
+
+function App() {
+  return (
+    <div>
+      <TelaInicial/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <ListaDeCartas/>
+    </div>
+  )
+}
+
+export default App
---
+++ JogoDeBaralho/src/components/TelaInicial/TelaInicial.jsx
@@ -0,0 +1,11 @@
+import Titulo from "../Titulo/Titulo"
+import Jogo from "../Jogo/Jogo"
+
+export default function TelaInicial() {
+    return (
+        <>
+            <Titulo />
+            <Jogo />
+        </>
+    )
+}
