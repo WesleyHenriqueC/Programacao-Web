8cf83bbd6de0bd89d66f0d198eafafc7cd58eac9
WesleyHenriqueC <95238638+WesleyHenriqueC@users.noreply.github.com>
2023-12-05 21:31:34 -0300

    Delete JogoDeBaralho directory

--- JogoDeBaralho/src/components/Regras/Titulo.jsx
+++
@@ -1,9 +0,0 @@
-import "./Titulo.css";
-
-export default function Titulo(){
-    return(
-        <div className="box">
-            <p className="texto">Regras</p>
-        </div>
-    );
-}
--- JogoDeBaralho/src/components/botaoRegras/botaoRegras.jsx
+++
@@ -1,9 +0,0 @@
-import "./botaoRegras.css";
-
-export default function porcariadoBotao(){
-    return(
-    <div className="areaBotaoVolt">
-        <button>Regras</button>
-    </div>)
-}
-
